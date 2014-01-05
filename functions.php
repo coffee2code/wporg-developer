0ff627314c5319812922f47bfa04d182d5b34cc6
Andrey Savchenko <contact@rarst.net>
2014-01-05 17:01:29 +0200

    Added text domain to translation calls in functions.php

--- functions.php
+++ functions.php
@@ -35,7 +35,7 @@ function wp_doc_comment( $comment, $args, $depth ) {
 		<article id="comment-<?php comment_ID(); ?>" class="comment">
 
 			<?php if ( $comment->comment_approved == '0' ) : ?>
-				<em class="comment-awaiting-moderation"><?php _e('Your example is awaiting moderation.'); ?></em>
+				<em class="comment-awaiting-moderation"><?php _e( 'Your example is awaiting moderation.', 'wporg' ); ?></em>
 				<br />
 			<?php endif; ?>
 
@@ -47,18 +47,18 @@ function wp_doc_comment( $comment, $args, $depth ) {
 						echo get_avatar( $comment );
 
 						/* translators: 1: comment author, 2: date and time */
-						printf( __('Contributed by %1$s on %2$s'),
+					printf( __( 'Contributed by %1$s on %2$s', 'wporg' ),
 							sprintf( '<span class="fn">%s</span>', get_comment_author_link() ),
 							sprintf( '<a href="%1$s"><time datetime="%2$s">%3$s</time></a>',
 								esc_url( get_comment_link( $comment->comment_ID ) ),
 								get_comment_time( 'c' ),
 								/* translators: 1: date, 2: time */
-								sprintf( __('%1$s at %2$s'), get_comment_date(), get_comment_time() )
+									sprintf( __( '%1$s at %2$s', 'wporg' ), get_comment_date(), get_comment_time() )
 							)
 						);
 					?>
 
-					<?php edit_comment_link( __('Edit'), '<span class="edit-link">', '</span>' ); ?>
+					<?php edit_comment_link( __( 'Edit', 'wporg' ), '<span class="edit-link">', '</span>' ); ?>
 				</div><!-- .comment-author .vcard -->
 
 			</footer>
