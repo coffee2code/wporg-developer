4c1e2ffe0116c1afe40d691776f348f2a09d8766
Andrey Savchenko <contact@rarst.net>
2014-01-14 00:13:10 +0200

    Moved function content to the content  template part.

--- archive-wpapi-function.php
+++ archive-wpapi-function.php
@@ -12,33 +12,8 @@
 				<hr />
 			<?php endif; ?>
 
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<?php get_template_part( 'content', get_post_type() ); ?>
 
-				<h1><a href="<?php the_permalink() ?>"><?php echo get_signature(); ?></a></h1>
-
-				<section class="description">
-					<?php the_excerpt(); ?>
-				</section>
-
-				<section class="long-description">
-					<?php the_content(); ?>
-				</section>
-
-				<section class="return"><strong>Return:</strong> <?php echo get_return(); ?></section>
-
-				<?php
-				$since = get_since();
-				if ( ! empty( $since ) ) :
-					?>
-					<section class="since">
-						<strong>Since:</strong> WordPress
-						<a href="<?php echo get_since_link( $since ); ?>"><?php echo $since; ?></a>
-					</section>
-				<?php endif; ?>
-
-				<section class="meta">Used by TODO | Uses TODO | TODO Examples</section>
-
-			</article>
 		<?php endwhile; ?>
 
 		<?php else : ?>
