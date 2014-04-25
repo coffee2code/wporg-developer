d293582cb42a221ae7ef0fc74b0247e8d1499d21
Scott Reilly <scott@coffee2code.com>
2014-04-25 15:02:36 -0500

    List methods for a class on the class's page

--- content-reference.php
+++ content-reference.php
@@ -88,6 +88,32 @@ if ( ! empty( $since ) ) : ?>
 		<h2><?php _e( 'Examples', 'wporg' ); ?></h2>
 	</section>
 	-->
+
+	<?php if ( 'wp-parser-class' === get_post_type() ) :
+		if ( $children = get_children( array( 'post_parent' => get_the_ID(), 'post_status' => 'publish' ) ) ) :
+			usort( $children, __NAMESPACE__ . '\\compare_objects_by_name' );
+	?>
+		<hr/>
+		<section class="class-methods">
+		<h2><?php _e( 'Methods', 'wporg' ); ?></h2>
+		<ul>
+			<?php foreach ( $children as $child ) : ?>
+				<li><a href="<?php echo get_permalink( $child->ID ); ?>">
+				<?php
+					$title = get_the_title( $child );
+					$pos = ( $i = strpos( $title, ':' ) ) ? $i + 1 : 0;
+					echo substr( $title, $pos );
+				?></a>
+				<?php if ( $excerpt = apply_filters( 'get_the_excerpt', $child->post_excerpt ) ) {
+					echo '&mdash; ' . sanitize_text_field( $excerpt );
+				} ?>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+		</section>
+		<?php endif;
+	endif; ?>
+
 <?php endif; ?>
 
 </article>
