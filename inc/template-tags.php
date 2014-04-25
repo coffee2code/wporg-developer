e167c43aa24e75733b9084910e366131df334c1e
Nicole Arnold <nicole@alleyinteractive.com>
2014-04-25 08:45:53 -0400

    remove unneeded function

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -287,27 +287,6 @@ namespace DevHub {
 				return 'Developer Resources';
 		}
 	}
-	/**
-	 * Get template part name
-	 *
-	 * @return string
-	 */
-	function get_template_part_name( $post_type = null ) {
-		if ( empty( $post_type ) ) {
-			$post_type = get_post_type();
-		}
-		$handbooks = array( 'theme-handbook', 'plugin-handbook' );
-		$reference = array( 'wp-parser-class', 'wp-parser-function', 'wp-parser-method' );
-		if ( in_array( $post_type, $reference ) ) {
-			$part = 'reference';
-		} elseif ( in_array( $post_type, $handbooks ) ) {
-			$part = 'handbook';
-		} else {
-			$part = $post_type;
-		}
-
-		return $part;
-	}
 
 	/**
 	 * Get post type name
